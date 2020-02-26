77e7eea9d4e9cc75b026c3abd2d0ef45edb38174
Joo Suhong <rodney.joo@gmail.com>
2020-02-26 22:59:57 +0900

    Update vipsister23_test.py

--- scraper/test/vipsister23_test.py
+++ scraper/test/vipsister23_test.py
@@ -7,12 +7,14 @@ class TestScraper(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.sc = vipsister23.Scraper()
+        cls.test_url = 'http://vipsister23.com/archives/9564571.html'
+        cls.url_regex_pattern = 'https://livedoor.blogimg.jp/vipsister23/imgs/[0-9a-z/]+.jpg'
 
     def test_get_imges_from_url(self):
-        title, imgs = self.sc.get_imges_from_url('http://vipsister23.com/archives/9564571.html')
+        title, imgs = self.sc.get_imges_from_url(self.test_url)
         self.assertGreater(len(imgs), 0)
         for img in imgs:
-            self.assertRegex(img, 'https://livedoor.blogimg.jp/vipsister23/imgs/[0-9a-z/]+.jpg')
+            self.assertRegex(img, self.url_regex_pattern)
 
     def test_get_title(self):
         html = f'<html>' \
