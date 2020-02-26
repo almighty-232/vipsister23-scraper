dcd1f5d41f29af142bb0d6c152440381ba87d6ff
Joo Suhong <rodney.joo@gmail.com>
2020-02-26 22:22:39 +0900

    Summary

--- manager/downloader.py
+++ manager/downloader.py
@@ -15,10 +15,15 @@ def download(path: str, url: str, subdir: str = None):
         os.makedirs(path, exist_ok=True)
     # extract file name from url
     re_pattern = '[0-9a-z]+.(jpg|png|gif)'
-    nm = re.search(re_pattern, url).group()
-    # file name
-    f_name = f'{path}/{nm}'
-    # download
-    print(f'Downloading... {f_name}')
-    urllib.request.urlretrieve(url, f_name)
+    regex_result = re.search(re_pattern, url)
+    if not regex_result:
+        type_nm = url.split('.')[-1]
+        print(f'Error: Not Supported Multimedia Type : {type_nm} Url : {url}')
+    else:
+        nm = regex_result.group()
+        # file name
+        f_name = f'{path}/{nm}'
+        # download
+        print(f'Downloading... {f_name}')
+        urllib.request.urlretrieve(url, f_name)
 
