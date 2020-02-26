b98c1622524dffd861352ae6bda1691b57bb726f
Joo Suhong <rodney.joo@gmail.com>
2020-02-26 22:34:36 +0900

    Create scraper.py

---
+++ scraper/scraper.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractmethod
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+class ScrapeWebsite(metaclass=ABCMeta):
+    url = None
+
+    def __init__(self, url: str):
+        self.url = url
+
+    def get_imges_from_url(self, url: str):
+        """
+
+        :param url: str
+            scraper's url
+        :return: tuple
+            page's title, image(list)
+        """
+        if not url:
+            raise ValueError('url is empty')
+
+        res = requests.get(url)
+        if res.status_code != 200:
+            raise requests.HTTPError
+        # res.encoding = res.apparent_encoding
+        bsobj = bs(res.text, 'html.parser')
+
+        return self.get_title(bsobj), self.get_files(bsobj)
+
+    @abstractmethod
+    def get_title(self, obj: bs) -> str:
+        pass
+
+    @abstractmethod
+    def get_files(self, obj: bs) -> list:
+        pass
+
+
+if __name__ == '__main__':
+    # print(get_imges_from_url('http://vipsister23.com/archives/9564543.html'))
+    pass
+
